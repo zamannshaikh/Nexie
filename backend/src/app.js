f7669aec376c2f80f6f50832be43323181ea26ae
Zaman Shaikh <zamanshaikh2048@gmail.com>
2025-10-09 13:41:24 +0530

    `Update app.js to use a regular expression for serving index.html`

--- backend/src/app.js
+++ backend/src/app.js
@@ -34,8 +34,8 @@ app.use('/api/chats',chatRoutes);
 
 
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+app.get(/^(?!\/api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 
