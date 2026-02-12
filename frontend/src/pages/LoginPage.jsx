51717eb41ef62321c4712ece5c81ec251b42705e
Zaman Shaikh <zamanshaikh2048@gmail.com>
2026-02-12 15:56:36 +0530

    testing api config

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -6,6 +6,8 @@ import { asyncLoginUser, asyncLoginWithGoogle } from '../store/services/userServ
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
 import '../styles/LoginPage.css';
+import api from '../api/axiosconfig';
+console.log("API Base URL:", api.defaults.baseURL);
 
 // --- SVG Icons ---
 const EyeIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" /><circle cx="12" cy="12" r="3" /></svg>);
